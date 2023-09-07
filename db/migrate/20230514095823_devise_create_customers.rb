6b69e941241559e7fb7fd676940b877174196a1d
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-09-07 17:48:43 +0000

    カート内商品、注文、注文詳細テーブル作成

--- db/migrate/20230514095823_devise_create_customers.rb
+++ db/migrate/20230514095823_devise_create_customers.rb
@@ -40,19 +40,5 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
     # add_index :customers, :confirmation_token,   unique: true
     # add_index :customers, :unlock_token,         unique: true
   end
-  
-  def change
-    create_table :items do |t|
-      t.string :name, null: false
-      t.text :introduction, null: false
-      t.integer :price, null: false
-      
-      #以下、チャレンジ機能#
-      #t.integer :genre_id, null: false
-      #t.boolean :is_active, null: false
-      #ここまで#
-      
-      t.timestamps
-    end
-  end
+
 end
--- db/migrate/20230906165406_create_items.rb
+++ db/migrate/20230906165406_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :price, null: false
+      
+      #以下、チャレンジ機能#
+      #t.integer :genre_id, null: false
+      #t.boolean :is_active, null: false
+      #ここまで#
 
       t.timestamps
     end
