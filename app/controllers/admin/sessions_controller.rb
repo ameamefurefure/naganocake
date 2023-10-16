1e836f397aca034271ab7dc3937c09ccd9e56bbf
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-10-16 17:16:41 +0000

    ログイン画面表示修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  #before_action :configure_sign_in_params, only: [:create]
 
-  def after_sign_up_path_for(resource)
+  def after_sign_in_path_for(resource)
     root_path
   end
   
