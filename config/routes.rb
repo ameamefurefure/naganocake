e71b0965689cc95b64eb8fc4387eeaa0b2c4c84d
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-10-17 17:53:19 +0000

    171日修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,18 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords], controllers:{
   sessions: "admin/sessions"
 }
+
+#会員側のルーティング設定
+get 'items' =>'public/items#index'
+#管理者側のルーティング設定
+namespace :admin do
+  resources :items, only: [:index]
+end
+
 end
