2dd3571f95503e91938ef049f2d5c8b2e8e23132
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-10-19 17:58:51 +0000

    10.19時点

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,7 +4,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
-    user_path(current_user.id)
+    customers_path(current_customers.id)
   end
 
   protected
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,7 +4,7 @@ class Public::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    customers_path(current_customer.id)
   end
 
   def after_sign_out_path_for(resource)
