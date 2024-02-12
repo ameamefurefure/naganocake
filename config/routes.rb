46fcb9723115875ee60bf2f1c54de4007d6416bd
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2024-02-12 17:28:14 +0000

    画面追加(管理者_商品詳細画面まで)

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,12 @@ devise_for :admin, skip: [:registrations, :passwords], controllers:{
 #会員側のルーティング設定
 get 'items' =>'public/items#index'
 get 'customers' =>'public/customers#show'
+
 #管理者側のルーティング設定
 namespace :admin do
   resources :items, only: [:index]
 end
-
+get 'admin/items/new' =>'admin/items#new'#商品新規登録画面
+get 'admin/items' =>'admin/items#index'#商品一覧画面
+get 'admin/items/:id' =>'admin/items#show'#商品詳細画面
 end
