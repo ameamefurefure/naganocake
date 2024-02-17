7565a1d4e2105093920e3d0fd69a5bc8d331ef68
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2024-02-17 18:31:41 +0000

    画面追加(管理者_トップ画面まで)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #namespace :admin do
+   # get 'homes/top'
+  #end
+  
   # devise_for :admins
   # devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -25,10 +29,16 @@ get 'customers' =>'public/customers#show'
 namespace :admin do
   resources :items, only: [:index]
   resources :customers, only: [:index]
+  resources :homes, only: [:top]
 end
+
 get 'admin/items/new' =>'admin/items#new'#商品新規登録画面
 get 'admin/items' =>'admin/items#index'#商品一覧画面
 get 'admin/items/:id' =>'admin/items#show'#商品詳細画面
 get 'admin/items/:id/edit' =>'admin/items#edit'#商品編集画面
 get 'admin/customers' =>'admin/customers#index'#顧客一覧画面
+get 'admin/customers/:id' =>'admin/customers#show'#顧客詳細画面
+get 'admin/customers/:id/edit' =>'admin/customers#edit'#顧客編集画面
+get 'admin' =>'admin/homes#top'#管理者トップ画面
+
 end
