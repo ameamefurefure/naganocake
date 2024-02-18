681f34ee87f4c050e8c3e31c78900fea2aa470da
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2024-02-18 11:40:32 +0000

    画面追加(ECサイト_登録情報編集画面まで)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #namespace :admin do
    # get 'homes/top'
   #end
-  
+
   # devise_for :admins
   # devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -22,23 +22,27 @@ devise_for :admin, skip: [:registrations, :passwords], controllers:{
 }
 
 #会員側のルーティング設定
-get 'items' =>'public/items#index'
+get 'items' =>'public/items#index'#
 get 'customers' =>'public/customers#show'
+
+get '/' =>'public/homes#top'#ECサイト側_トップページ
+get 'about' =>'public/homes#about'#ECサイト側_アバウトページ
+get 'customers/information/edit' =>'public/customers#edit'#ECサイト側_顧客の登録情報編集画面
 
 #管理者側のルーティング設定
 namespace :admin do
-  resources :items, only: [:index]
-  resources :customers, only: [:index]
-  resources :homes, only: [:top]
+  resources :items, only: [:new, :index, :show, :edit]
+  resources :customers, only: [:index, :show, :edit]
 end
 
-get 'admin/items/new' =>'admin/items#new'#商品新規登録画面
-get 'admin/items' =>'admin/items#index'#商品一覧画面
-get 'admin/items/:id' =>'admin/items#show'#商品詳細画面
-get 'admin/items/:id/edit' =>'admin/items#edit'#商品編集画面
-get 'admin/customers' =>'admin/customers#index'#顧客一覧画面
-get 'admin/customers/:id' =>'admin/customers#show'#顧客詳細画面
-get 'admin/customers/:id/edit' =>'admin/customers#edit'#顧客編集画面
+# resourcesメソッドにより下記コメントアウト
+# get 'admin/items/new' =>'admin/items#new'#管理者側_商品新規登録画面
+# get 'admin/items' =>'admin/items#index'#管理者側_商品一覧画面
+# get 'admin/items/:id' =>'admin/items#show'#管理者側_商品詳細画面
+# get 'admin/items/:id/edit' =>'admin/items#edit'#管理者側_商品編集画面
+# get 'admin/customers' =>'admin/customers#index'#管理者側_顧客一覧画面
+# get 'admin/customers/:id' =>'admin/customers#show'#管理者側_顧客詳細画面
+# get 'admin/customers/:id/edit' =>'admin/customers#edit'#管理者側_顧客編集画面
 get 'admin' =>'admin/homes#top'#管理者トップ画面
 
 end
