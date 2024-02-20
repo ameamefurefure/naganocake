ad3783b7f46535f4d27457c08f0603e30064d084
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2024-02-20 11:52:01 +0000

    画面追加(ECサイト_注文履歴詳細画面まで)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   #namespace :public do
+    #get 'orders/new'
+    #get 'orders/confirm'
+    #get 'orders/thanks'
+    #get 'orders/index'
+    #get 'orders/show'
+  #end
+
+  #namespace :public do
     #get 'cart_items/index'
   #end
   #namespace :admin do
@@ -35,6 +43,12 @@ get 'customers/quit' =>'public/customers#quit'#ECサイト側_顧客の退会確
 get 'items' =>'public/items#index'#ECサイト側_商品一覧画面
 get 'items/:id' =>'public/items#show'#ECサイト側_商品詳細画面
 get 'cart_items' =>'public/cart_items#index'#ECサイト側_カート画面
+
+get 'orders/new' =>'public/orders#new'#ECサイト側_注文情報入力画面
+get 'orders/confirm' =>'public/orders#confirm'#ECサイト側_注文情報確認画面
+get 'orders/thanks' =>'public/orders#thanks'#ECサイト側_注文完了（サンクス）画面
+get 'orders' =>'public/orders#index'#ECサイト側_注文履歴画面
+get 'orders/:id' =>'public/orders#show'#ECサイト側_注文履歴詳細画面
 
 #管理者側のルーティング設定
 namespace :admin do
